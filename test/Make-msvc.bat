c78063fb9c2d68d2b3dc5dec079f4116d6896b89
Andrei Lapets <a@lapets.io>
2020-07-18 02:54:17 -0400

    Add Windows build/tests to Travis CI configuration.

--- test/Make-msvc.bat
+++ test/Make-msvc.bat
@@ -1,9 +1,12 @@
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+
 @IF EXIST *.obj @DEL *.obj
 @IF EXIST target\*.exp @DEL target\*.exp
 @IF EXIST target\*.lib @DEL target\*.lib
 @IF EXIST target\*.l @DEL target\*.l
 
-copy /y NUL target\test.error.invalid.l >NUL
+COPY /y NUL target\test.error.invalid.l >NUL
 cl source/test.error.none.cpp /nologo /LD /Fetarget/test.error.none.l
 cl source/test.error.logic.cpp /nologo /LD /Fetarget/test.error.logic.l
 cl source/test.error.runtime.cpp /nologo /LD /Fetarget/test.error.runtime.l
